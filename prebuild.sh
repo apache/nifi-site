a9c548c0cbc500fb33354704b67c70898de634c4
exceptionfactory <exceptionfactory@apache.org>
2025-02-01 21:12:02 -0600

    NIFI-14221 Added REST API Version 2 Documentation

--- prebuild.sh
+++ prebuild.sh
@@ -15,6 +15,11 @@ MANIFEST_DOCS_DIR=docs
 NIFI_DOCS_ZIP_URL="$MAVEN_BASE_URL/org/apache/nifi/nifi-docs/$CURRENT_VERSION/nifi-docs-$CURRENT_VERSION-resources.zip"
 NIFI_DOCS_ZIP_PATH=$RESOURCES_DIR/nifi-docs-resources.zip
 
+NIFI_WEB_API_WAR_URL="$MAVEN_BASE_URL/org/apache/nifi/nifi-web-api/$CURRENT_VERSION/nifi-web-api-$CURRENT_VERSION.war"
+NIFI_WEB_API_WAR_PATH=$RESOURCES_DIR/nifi-web-api.war
+REST_API_INDEX_HTML=docs/rest-api/index.html
+REST_API_SWAGGER_YAML=docs/rest-api/swagger.yaml
+
 # Create Hugo directories
 if [ ! -d $RESOURCES_DIR ];then
   mkdir $RESOURCES_DIR
@@ -29,6 +34,13 @@ unzip -q -o -d $PREBUILD_DIR/assets $MANIFEST_JAR_PATH $MANIFEST_JSON $MANIFEST_
 echo "Downloading $NIFI_DOCS_ZIP_URL"
 curl -o $NIFI_DOCS_ZIP_PATH $NIFI_DOCS_ZIP_URL
 unzip -q -o -d $PREBUILD_DIR/public $NIFI_DOCS_ZIP_PATH
+
+# Download REST API Documentation Resources and extract to prebuild
+echo "Downloading $NIFI_WEB_API_WAR_URL"
+curl -o $NIFI_WEB_API_WAR_PATH $NIFI_WEB_API_WAR_URL
+unzip -q -o -d $PREBUILD_DIR/public $NIFI_WEB_API_WAR_PATH $REST_API_INDEX_HTML $REST_API_SWAGGER_YAML
+mv $PREBUILD_DIR/public/$REST_API_INDEX_HTML $PREBUILD_DIR/public/html/rest-api.html
+mv $PREBUILD_DIR/public/$REST_API_SWAGGER_YAML $PREBUILD_DIR/public/html/swagger.yaml
 
 # Build components using prebuild directory with Hugo
 hugo -s $PREBUILD_DIR
